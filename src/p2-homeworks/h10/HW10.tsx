ea10f5a6bda62b71fe5473019400cf4d6d2d7b3b
Timmy2324 <salikhov.t.s@yandex.ru>
2022-02-15 23:59:14 +0300

    homework 10

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "./bll/store";
+import {loadingAC} from "./bll/loadingReducer";
+import s from './HW10.module.css';
 
 function HW10() {
-    // useSelector, useDispatch
-    const loading = false
+    const loading = useSelector<AppStoreType, boolean>(state => state.loading.isLoading);
+    const dispatch = useDispatch()
 
     const setLoading = () => {
-        // dispatch
-        // setTimeout
+        dispatch(loadingAC(true))
+        setTimeout(() => {
+            dispatch(loadingAC(false))
+        }, 2000);
         console.log('loading...')
     };
 
@@ -19,7 +25,25 @@ function HW10() {
             {/*should work (должно работать)*/}
             {loading
                 ? (
-                    <div>крутилка...</div>
+                    <div className={s.circleWrapper}>
+                        <div className={s.windows8}>
+                            <div className={s.wBall} id={s.wBall_1}>
+                                <div className={s.wInnerBall}></div>
+                            </div>
+                            <div className={s.wBall} id={s.wBall_2}>
+                                <div className={s.wInnerBall}></div>
+                            </div>
+                            <div className={s.wBall} id={s.wBall_3}>
+                                <div className={s.wInnerBall}></div>
+                            </div>
+                            <div className={s.wBall} id={s.wBall_4}>
+                                <div className={s.wInnerBall}></div>
+                            </div>
+                            <div className={s.wBall} id={s.wBall_5}>
+                                <div className={s.wInnerBall}></div>
+                            </div>
+                        </div>
+                    </div>
                 ) : (
                     <div>
                         <SuperButton onClick={setLoading}>set loading...</SuperButton>
