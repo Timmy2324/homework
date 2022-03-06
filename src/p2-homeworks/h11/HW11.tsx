682404c892091706b4c462a0bdd07d459989412b
Timmy2324 <salikhov.t.s@yandex.ru>
2022-03-07 01:23:47 +0300

    homework 11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -6,6 +6,15 @@ function HW11() {
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
 
+    const onChangeRange = (newValue: number | number[]) => {
+      if(typeof newValue === 'number') {
+          setValue1(newValue);
+      } else {
+          setValue1(newValue[0]);
+          setValue2(newValue[1]);
+      }
+    }
+
     return (
         <div>
             <hr/>
@@ -15,15 +24,22 @@ function HW11() {
             <div>
                 <span>{value1}</span>
                 <SuperRange
+                    value={value1}
+                    onChangeRange={setValue1}
                     // сделать так чтоб value1 изменялось
                 />
             </div>
 
-            <div>
+            <div style={{display: "flex", alignItems: 'center'}}>
                 <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
-                />
+                <span style={{display: "inline-block", width: "100px"}}>
+                    <SuperDoubleRange
+                        value={[value1, value2]}
+                        onChangeRange={onChangeRange}
+                        // сделать так чтоб value1 и value2 изменялось
+                    />
+                </span>
+
                 <span>{value2}</span>
             </div>
 
