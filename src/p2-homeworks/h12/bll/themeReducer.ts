19672bebbe36bd8428fe5e388422342542fe0d38
Timmy2324 <salikhov.t.s@yandex.ru>
2022-03-29 20:47:59 +0300

    homework 12

--- src/p2-homeworks/h12/bll/themeReducer.ts
+++ src/p2-homeworks/h12/bll/themeReducer.ts
@@ -1,14 +1,35 @@
-const initState = {
 
+export enum Theme {
+    dark = 'dark',
+    red = 'red',
+    some = 'some',
+}
+
+type StateType = {
+    theme: Theme
+}
+
+const initState: StateType = {
+    theme: Theme.some
 };
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state: StateType = initState, action: ActionType): StateType => { // fix any
     switch (action.type) {
-        case "": {
-            return state;
+        case "CHANGE-THEME": {
+            return {...state, theme: action.payload.theme};
         }
         default: return state;
     }
 };
 
-export const changeThemeC = (): any => {}; // fix any
+type ActionType = ChangeThemeType;
+
+type ChangeThemeType = ReturnType<typeof changeTheme>
+export const changeTheme = (theme: Theme) => {
+    return {
+        type: 'CHANGE-THEME',
+        payload: {
+            theme
+        }
+    } as const
+};
